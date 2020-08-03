c7c01e7b6d3dc6cefefbeb9d3364441d40fa3532
Sienna Kopf <princess.kopf@gmail.com>
2020-08-03 10:10:26 -0600

    Add route for passenger show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/flights/:flight_id", to: "flights#show"
+  
+  get "/passengers/:passenger_id", to: "passengers#show"
 end

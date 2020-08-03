58c296509335e8e2e559bd63a0ec533bfa706d00
Sienna Kopf <princess.kopf@gmail.com>
2020-08-03 11:12:15 -0600

    Add model methods for counting minors and adults

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :passenger_flights
   has_many :passengers, through: :passenger_flights
+
+  def count_minors
+    passengers.where('age < ?', 18).count
+  end
+
+  def count_adults
+    passengers.where('age >= ?', 18).count
+  end
 end

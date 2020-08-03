bb5a524c01ce8e4165423eb2e8014219f20292b7
Sienna Kopf <princess.kopf@gmail.com>
2020-08-03 10:33:19 -0600

    Add model method test for add flight

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Passenger do
     it {should have_many :passenger_flights}
     it {should have_many(:flights).through(:passenger_flights)}
   end
+
+  describe 'methods' do
+    before :each do
+      @delta= Airline.create!(name: "Delta")
+      @flight_1 = @delta.flights.create!(number: "1727", date: "08/06/20", time: "13:30", departure_city: "Denver", arrival_city: "Munich")
+      @flight_2 = @delta.flights.create!(number: "1837", date: "08/09/20", time: "01:45", departure_city: "Munich", arrival_city: "Atlanta")
+
+      @passenger = Passenger.create!(name: "Cece", age: 18)
+    end
+    it '#add_flight' do
+      expect(@passenger.flights).to eq([])
+
+      @passenger.add_flight(@flight_1)
+      expect(@passenger.flights).to eq([@flight_1])
+      @passenger.add_flight(@flight_2)
+      expect(@passenger.flights).to eq([@flight_1, @flight_2])
+    end
+  end
 end

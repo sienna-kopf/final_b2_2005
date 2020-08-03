54bdeac6af869a6075c199c5848c325ef7fdc40f
Sienna Kopf <princess.kopf@gmail.com>
2020-08-03 11:13:13 -0600

    Add passengers to flight model test for counting adults and minors

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -15,4 +15,26 @@ RSpec.describe Flight do
     it {should have_many :passenger_flights}
     it {should have_many(:passengers).through(:passenger_flights)}
   end
+
+  describe 'methods' do
+    before :each do
+      @airline = Airline.create!(name: "Delta")
+      @flight_1 = @airline.flights.create!(number: "1727", date: "08/06/20", time: "13:30", departure_city: "Denver", arrival_city: "Munich")
+      @passenger_1 = @flight_1.passengers.create!(name: "Carlo", age: 55)
+      @passenger_2 = @flight_1.passengers.create!(name: "Joanne", age: 50)
+      @passenger_3 = @flight_1.passengers.create!(name: "Sarina", age: 20)
+      @passenger_4 = @flight_1.passengers.create!(name: "John", age: 75)
+      @passenger_5 = @flight_1.passengers.create!(name: "Zoey", age: 7)
+      @passenger_5 = @flight_1.passengers.create!(name: "Lily", age: 17)
+      @passenger_5 = @flight_1.passengers.create!(name: "Sienna", age: 18)
+    end
+
+    it "#count_minors" do
+      expect(@flight_1.count_minors).to eq(2)
+    end
+
+    it "#count_adults" do
+      expect(@flight_1.count_adults).to eq(5)
+    end
+  end
 end

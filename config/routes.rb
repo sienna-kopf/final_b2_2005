37e519e010e1205e5833e6e7eead7696989511be
Sienna Kopf <princess.kopf@gmail.com>
2020-08-03 10:35:18 -0600

    Add route for passenger flights create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/flights/:flight_id", to: "flights#show"
-  
+
   get "/passengers/:passenger_id", to: "passengers#show"
+
+  post "/passenger_flights", to: "passenger_flights#create"
 end
